84a294b4f321c9116162e5e93eba030dcb9dcd29
lukam <lukamiskin06@gmail.com>
2026-06-07 13:48:17 +0300

    username now must be unique - dm25047

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'username' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
             'email'    => 'required|email|unique:users',
             'password' => 'required|confirmed|min:6',
         ]);
